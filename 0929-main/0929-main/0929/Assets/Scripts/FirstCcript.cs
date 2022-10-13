628f6fa82cf933eb21f783248f39eb31ffaf27ce
LucienQian <jean20011116@gmail.com>
2022-10-13 16:02:57 +0800

    10013

--- 0929-main/0929-main/0929/Assets/Scripts/FirstCcript.cs
+++ 0929-main/0929-main/0929/Assets/Scripts/FirstCcript.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+namespace LUCIEN
+{ 
+/// <summary>
+/// 第一個程式
+/// </summary>
 public class FirstCcript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        #region 註解區域
+        //縮排 快捷鍵 Tab
+        //格式化文件 Ctrl + K D
+        //單行註解
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /*
+         * 多行註解
+         * 多行註解
+         */
+        #endregion
+
+# region 事件區域
+        //喚醒事件：遊戲開始時執行一次
+        private void Awake()
+        {
+            
+        }
+
+        //開始事件：在 Awake 後執行一次
+        private void Start()
+        {
+            
+        }
+        #endregion
     }
 }
